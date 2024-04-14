91b6cb65c6c188984ef7e2a23f29d5ea606d5790
SonMyungJi <linolousky@gmail.com>
2024-04-14 11:48:41 +0900

    title도 가져올 수 있나

--- services/notion.js
+++ services/notion.js
@@ -29,6 +29,7 @@ async function getSports() {
 
   const rawData = results.map(page => {
     return {
+      "title": page.properties.title.title,
       "date": new Date(page.created_time),
       "sport": page.properties.Sports.select.name,
       "sun": page.properties.SUN.checkbox ? 1 : 0,
