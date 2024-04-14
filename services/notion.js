378de3e0b7911d92576edf450ad947dd9507337f
SonMyungJi <linolousky@gmail.com>
2024-04-14 22:12:29 +0900

    코드 리팩토링

--- services/notion.js
+++ services/notion.js
@@ -25,7 +25,7 @@ async function getTodos() {
 
   const rawData = results.map(page => {
     return {
-      "name": page.properties.Todo.title,
+      "name": page.properties.Todo.title[0].text.content,
       "date": new Date(page.created_time),
       "sun": page.properties.SUN.checkbox ? 1 : 0,
       "mon": page.properties.MON.checkbox ? 1 : 0,
